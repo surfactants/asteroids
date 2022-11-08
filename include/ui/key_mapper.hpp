9f615c044308cad62d10783d1ea7f69fbaa62523
surfactants <8463818+surfactants@users.noreply.github.com>
2022-11-08 04:54:02 -0500

    cleaned Key_Mapper docs
    also made it compatible with non-polymorphism in case i want to make that switch

--- include/ui/key_mapper.hpp
+++ include/ui/key_mapper.hpp
@@ -7,54 +7,15 @@
 #include <input/key_char.hpp>
 #include <input/mouse_event.hpp>
 
-//this will work great, i think, at the expense of a less generic input handler
-
-//TODO:
-// scrolling ? :(
-// re-setting a key should swap with the previous binding (h/t oblivion)
-// buttons (confirm, cancel, reset changes)
-
 //recommend running checkMouse() every frame, for post-selection hovers
-
-//need to figure out a better bridge to the input handler
-//perhaps setting up an enum there?
-
-/* EXAMPLE ************************************************************************
- *
- * Key_Mapper key_mapper(font);
- * key_mapper.setPosition(sf::Vector2f(128.f, 128.f));
- * std::vector<Action> actions;
- *     actions.push_back(Action("move north", std::function<void()>(), sf::Keyboard::W));
- *     actions.push_back(Action("move west", std::function<void()>(), sf::Keyboard::A));
- *     actions.push_back(Action("move south", std::function<void()>(), sf::Keyboard::S));
- *     actions.push_back(Action("move east", std::function<void()>(), sf::Keyboard::D));
- * key_mapper.setActions(actions);
- *
- **********************************************************************************/
-
-// constructor (pass font ref)
-// reset -
-// setPosition - places the object - no origin is defined, so this is the top-left corner
-// checkMouse() - updates highlight state. returns true if the mouse is in the object.
-// clickLeft() - parses left mouse button press, used to select rows.
-// keyPressed() - updates key of selected row, or cancels if it's Escape
-// setTextColor()
-// setBackgroundColor()
-// setHoverColor()
-// setSelectColor()
-// setActions() - create rows from a vector of Action objects.
-// getActions() - returns a vector of actions for linking to the input handler
 
 /* PUBLIC METHODS *****************************************************************
  *
- * Key_Mapper(sf::Font& font)
- *     No default constructor
- *
- * void setActions(const std::vector<Action>& actions
- *     Creates rows from action class (name/function/key)
+ * void setActions(const sf::Font& font, const std::vector<Action>& actions
+ *     Creates rows from action class (name/function/trigger)
  *
  * std::vector<Action> getActions()
- *     ___
+ * 
  *
  * void reset()
  *     unhovers, unselects, and reverts changes
@@ -66,38 +27,58 @@
  *     places the object - no origin is defined, so this is the top-left corner
  *
  * bool checkMouse(sf::Vector2i mpos)
- *     ___
+ *     updates highlight state. returns true if the mouse is in the object.
  *
  * bool clickLeft()
- *     ___
+ *     for selecting rows and setting input. returns true if parsed.
+ * 
+ * bool clickRight()
+ *     for setting input. returns true if parsed
  *
  * bool keyPressed(sf::Keyboard::Key key)
- *     ___
+ *     sets input, if key != Escape. deselects regardless.
  *
  * void setTextColor(const sf::Color& color)
- *     ___
+ *     for all row styles
  *
  * void setBackgroundColor(const sf::Color& color)
- *     ___
+ *     for regular rows
  *
  * void setHoverColor(const sf::Color& color)
- *     ___
+ *     for moused rows
  *
  * void setSelectColor(const sf::Color& color)
- *     ___
+ *     for selected rows
  *
  **********************************************************************************/
 
+
+/* EXAMPLE ************************************************************************
+ *
+ * Key_Mapper key_mapper();
+ * key_mapper.setPosition(sf::Vector2f(128.f, 128.f));
+ * std::vector<Action> actions;
+ *     actions.push_back(Action("move north", std::function<void()>(), sf::Keyboard::W));
+ *     actions.push_back(Action("move west", std::function<void()>(), sf::Keyboard::A));
+ *     actions.push_back(Action("move south", std::function<void()>(), sf::Keyboard::S));
+ *     actions.push_back(Action("move east", std::function<void()>(), sf::Keyboard::D));
+ * 
+ * sf::font font;
+ * font.loadFromFile(" ... ");
+ * key_mapper.setActions(font, actions);
+ *
+ **********************************************************************************/
+
+
 class Key_Mapper : public sf::Drawable {
 public:
+    Key_Mapper();
+
     std::vector<Action> getActions();
 
     bool keyPressed(sf::Keyboard::Key key);
 
     void reset();
-
-protected:
-    Key_Mapper();
 
     void setActions(const sf::Font& font, const std::vector<Action>& actions);
 
@@ -115,6 +96,7 @@ protected:
     void setHoverColor(const sf::Color& color);
     void setSelectColor(const sf::Color& color);
 
+protected:
     class Row;
 
     Convert_Key converter;
